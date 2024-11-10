19827f7c198df23cbf6f0d5181c7a019fb85e901
HighTechIt <159155405+HighTechIt@users.noreply.github.com>
2024-11-10 21:29:50 +0500

    Changes

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -15,13 +15,13 @@ namespace VisaApplication
     {
 
         /// <summary>
-        /// HeadContent control.
+        /// logincontent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
+        protected global::System.Web.UI.WebControls.Button logincontent;
 
         /// <summary>
         /// MainContent control.
